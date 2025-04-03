96041d66ba7138ff78533246eefe64d029959380
Jasmin Al-Said <hials18@student.sdu.dk>
2025-04-03 21:01:27 +0200

    Added a title for recipe + added html for recipe navigation

---
+++ Data/script.js
@@ -0,0 +1,8 @@
+// Select the component with a specific ID
+const element = document.getElementById('square1');
+
+// Do something with the selected element
+if (element) {
+    element.textContent = 'Hello, World!';
+    element.style.color = 'blue';
+}
